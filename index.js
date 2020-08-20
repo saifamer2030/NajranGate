43029a4bc9891b527d6b7a3073a675ff89d15827
Saif Amer <seif.ealaan@gmail.com>
2020-08-20 23:18:10 +0200

    ww

---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.sendAlarmNotification = functions.database.ref('/Alarm/{recieverUid}/{pushid}')
+    .onWrite(async (change, context) => {
+        
+    //   const sendUid = context.params.senderUid;
+      const recUid = context.params.recieverUid;
+      const pushId = context.params.pushid;
+
+      // If un-follow we exit the function.
+      if (!change.after.val()) {
+        return console.log('PushId ', pushId , ' recieve : ' ,recUid);
+      }
+      console.log('PushId:', pushId);
+      console.log('reciever UID:', recUid);
+
+          const userName = admin.database().ref(`/userdata/${recUid}/cName`).once('value');
+          const msge = admin.database().ref(`/Alarm/${recUid}/${pushId}/cType`).once('value');
+
+          // Get the follower profile.
+        //   const getFollowerProfilePromise = admin.database()
+        //   ref(`/Alarm/{recieverUid}/{pushid}/cType`).once('value');
+
+        // const getFollowerProfilePromise = admin.database()
+        // .ref(`/Alarm/${recUid}/${pushId}/cType`).once('value');
+    
+        const results = await Promise.all([ msge ,userName ]);
+          const msg = results[0];
+          const sen = results[1];
+
+          const payload = {
+            notification:{
+                // title : `رسالة جديدة من ${sen.val()}`,
+                title : `بوابة نجران - لديك اشعار جديد`,
+                body : `${sen.val()} - ${msg.val()}`,
+                badge : '1',
+                icon: 'https://firebasestorage.googleapis.com/v0/b/souqnagran-49abe.appspot.com/o/logo%2Fsouqnagran.png?alt=media&token=8e8f8ff2-0855-4e67-9a44-aff2811448e7',
+                sound : 'default'
+            }
+        };
+    
+        return admin.database().ref(`/Fcm-Token/${recUid}`).once('value').then(allToken => {
+            if(allToken.val()){
+
+                console.log('inside', allToken.val().Token);
+
+               // const token = Object.keys(allToken.val());
+
+                var str =  allToken.val().Token;
+                console.log('token available str ',str);
+                return admin.messaging().sendToDevice(str,payload);
+            }else{
+                console.log('No token available');
+            }
+        });
+      
+
+
+      
+    });
